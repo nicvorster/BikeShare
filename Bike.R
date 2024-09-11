b4f8d3eefd1daa08a775c38b25ce6de9b6c4b999
Nic <nvorster@byu.edu>
2024-09-11 10:10:44 -0600

    Linear Regression Update

--- Bike.R
+++ Bike.R
@@ -6,6 +6,7 @@ library(DataExplorer)
 library(GGally)
 
 data <- vroom("train.csv")
+testData  <- vroom("test.csv")
 
 ##EDA 
 
@@ -36,3 +37,28 @@ tempcount <- ggplot(data = data, aes(x= temp, y= count)) +
 tempcount
 
 (weather + temp) / (humidity + tempcount)
+
+### LINEAR REGRESSION MODEL
+library(tidymodels)
+
+## Setup and Fit the Linear Regression Model
+my_linear_model <- linear_reg() %>% #Type of model
+  set_engine("lm") %>% # Engine = What R function to use
+  set_mode("regression") %>% # Regression just means quantitative response
+  fit(formula=count~ temp +weather + humidity + windspeed, data=data)
+
+## Generate Predictions Using Linear Model
+bike_predictions <- predict(my_linear_model,
+                            new_data=testData) # Use fit to predict11
+bike_predictions ## Look at the output
+
+## Format the Predictions for Submission to Kaggle
+kaggle_submission <- bike_predictions %>%
+bind_cols(., testData) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+## Write out the file
+vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
