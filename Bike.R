214c2eaa5d5046b35e8cb9ba9d9da29336a1893f
Nic <nvorster@byu.edu>
2024-09-18 10:33:13 -0600

    Penalty Predictions

--- Bike.R
+++ Bike.R
@@ -4,7 +4,7 @@ library(vroom)
 library(patchwork)
 library(DataExplorer)
 library(GGally)
-
+library(glmnet)
 data <- vroom("train.csv")
 testData  <- vroom("test.csv")
 
@@ -39,7 +39,7 @@ tempcount
 (weather + temp) / (humidity + tempcount)
 
 ### LINEAR REGRESSION MODEL
-library(tidymodels)
+
 
 ## Setup and Fit the Linear Regression Model
 my_linear_model <- linear_reg() %>% #Type of model
@@ -91,3 +91,99 @@ kaggle_submission2 <- bike_predictions %>%
 
 ## Write out the file
 vroom_write(x=kaggle_submission2, file="./PoisPreds.csv", delim=",")
+
+
+
+
+
+
+### CLEANING DATA 
+cleandata <- data %>% 
+  select(-casual , -registered) %>% 
+  mutate(count = log(count))
+view(cleandata)
+### Recipe
+bike_recipe <- recipe(count ~ ., data = cleandata) %>% 
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>% 
+  step_mutate(weather = factor(weather, levels = 1:3, labels = c("Dry", "Rainy", "Snowwy"))) %>% 
+  step_mutate(windspeed = ifelse(windspeed > 20, "Very Windy", ifelse(windspeed >10, "Slightly Windy", "Not Windy"))) %>%
+  step_mutate(windspeed = factor(windspeed)) %>% 
+  step_time(datetime, features=("hour")) %>% 
+  step_mutate(datetime_hour = factor(datetime_hour)) %>% 
+  step_mutate(season = factor(season)) %>% 
+  step_rm(datetime) %>% 
+  step_mutate(newtemp = (temp + atemp)/2) %>% 
+  step_rm(temp, atemp)
+prepped_recipe <- prep(bike_recipe)
+baked <- bake(prepped_recipe, new_data = testData)
+
+
+## Define a model
+lin_model <- linear_reg() %>%
+set_engine("lm") %>%
+set_mode("regression")
+
+## Combine into a Workflow and fit
+bike_workflow <- workflow() %>%
+add_recipe(bike_recipe) %>%
+add_model(lin_model) %>%
+fit(data=cleandata)
+
+## Run all the steps on test data
+lin_preds <- predict(bike_workflow, new_data = testData)
+lin_preds <- exp(lin_preds)
+
+## Kaggle Submission 3
+kaggle_submission3 <- lin_preds %>%
+  bind_cols(., testData) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+## Write out the file
+vroom_write(x=kaggle_submission3, file="./LinPreds.csv", delim=",")
+
+
+
+
+
+
+#### PENALIZED LINEAR REGRESSION ###
+penalized_recipe <- recipe(count ~ ., data = cleandata) %>% 
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>% 
+  step_mutate(weather = factor(weather, levels = 1:3, labels = c("Dry", "Rainy", "Snowwy"))) %>% 
+  step_mutate(windspeed = ifelse(windspeed > 20, "Very Windy", ifelse(windspeed >10, "Slightly Windy", "Not Windy"))) %>%
+  step_mutate(windspeed = factor(windspeed)) %>% 
+  step_time(datetime, features=("hour")) %>% 
+  step_mutate(datetime_hour = factor(datetime_hour)) %>% 
+  step_mutate(season = factor(season)) %>% 
+  step_rm(datetime) %>% 
+  step_mutate(newtemp = (temp + atemp)/2) %>% 
+  step_rm(temp, atemp) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_normalize(all_numeric_predictors())
+preppen_recipe <- prep(penalized_recipe)
+bakedpen <- bake(preppen_recipe, new_data = testData)
+
+# Penalized regression model
+preg_model <- linear_reg(penalty=0, mixture=1) %>% #Set model and tuning
+  set_engine("glmnet") # Function to fit in R
+
+preg_wf <- workflow() %>%
+add_recipe(penalized_recipe) %>%
+add_model(preg_model) %>%
+fit(data=cleandata)
+penpreds <- predict(preg_wf, new_data=testData)
+penpreds <- exp(penpreds)
+
+## Kaggle Submission 4
+kaggle_submission4 <- penpreds %>%
+  bind_cols(., testData) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+## Write out the file
+vroom_write(x=kaggle_submission4, file="./PenPreds.csv", delim=",")
